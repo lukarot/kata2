64918320f5ab1a68c7ab46c9214bbf837f6a744d
usuario <usuario@10.22.143.51>
2016-10-07 11:20:06 +0100

    kata2 v4 with class histogram and class genericas

--- src/kata2/histogram.java
+++ src/kata2/histogram.java
@@ -3,19 +3,19 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class histogram {
-    private final int[] data;
+public class histogram<T> {
+    private final T[] data;
 
-    public histogram(int[] data) {
+    public histogram(T[] data) {
         this.data=data;
     }
 
-    public int[] getData() {
+    public T[] getData() {
         return data;
     }
     
-    public Map<Integer,Integer> getHistogram(){
-        Map<Integer,Integer> histogram=new HashMap<>();
+    public Map<T,Integer> getHistogram(){
+        Map<T,Integer> histogram=new HashMap<>();
         for (int i = 0; i < data.length; i++) {
             if(!histogram.containsKey(data[i])){
                 histogram.put(data[i], 0);
