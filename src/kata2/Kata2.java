539fb3fd00b083600df902d3e3c2300695772e1e
usuario <usuario@10.22.143.51>
2016-10-07 11:18:58 +0100

    kata2 v4 with class histogram

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -5,7 +5,8 @@ import java.util.Map;
 
 public class Kata2 {
     public static void main(String[] args) {
-        int[] data={1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        //Integer[] data={1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
+        String[] data={"Ana","Luis","Ana","Ana","Rosa","Rosa"};
         histogram histo=new histogram(data);
         Map<Integer,Integer> histogr=histo.getHistogram();
         for (int i : histogr.keySet()) {
